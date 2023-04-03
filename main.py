a2ef6a3c9321823ae2123438d0d0ae2735981e56
Maciej Żyrek <prozyr@gmail.com>
2023-04-04 00:25:08 +0200

    QQ update

--- main.py
+++ main.py
@@ -3,39 +3,44 @@ import numpy as np
 
 # Za pomocą telefonu wyświetl szachownicę
 
-cap = cv2.VideoCapture(0)
-
+############### Filtracja obrazu ####################
 # Tworzenie maski filtru górnoprzepustowego
 kernel = np.array([[-1,-1,-1],
                    [-1, 9,-1],
                    [-1,-1,-1]])
 
 # Tworzenie macierzy dla operacji morfologicznej
-kernel_morph = np.ones((3,3), np.uint8)
+kernel_morph = np.ones((2,2), np.uint8)
+############### ################ ####################
+
+cap = cv2.VideoCapture(0)
 
 def bin_img(frame):
+    # wykrycie krawędzi
     edges = cv2.Canny(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY),100,200)
+    # uzupełnieine dziur
     edges = cv2.morphologyEx(edges, cv2.MORPH_DILATE, kernel_morph)
+    # szukanie konturów
     contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+    Vframe = frame
     # Wybór czworokątów, które reprezentują interesujący nas kształt
     quads = []
+    vector_area = []        # TODO: próba odległości kamery od szachownicy
     for contour in contours:
         area = cv2.contourArea(contour)
-        if area > 100:
+        if area > 1000:
+            vector_area = vector_area +  [area]
             perimeter = cv2.arcLength(contour, True)
             approx = cv2.approxPolyDP(contour, 0.1 * perimeter, True)
             if len(approx) == 4:
                 x, y, w, h = cv2.boundingRect(approx)
                 if abs(w - h) < 50:
                     quads.append(approx)
-
+    print(10000*8/(np.mean(vector_area))," mm") # TODO: próba odległości kamery od szachownicy
     # Narysowanie konturu czworokąta na oryginalnym obrazie
     for quad in quads:
-        cv2.drawContours(frame, [quad], 0, (0, 255, 0), 2)
-
-
-    Vframe = frame
+        cv2.drawContours(Vframe, [quad], 0, (0, 255, 0), 2)
 
     return Vframe, edges
 
