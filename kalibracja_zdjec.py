18230eaf4d0f6d4cd301c426f4b6b8ba4bd0c779
MavekGit <128191169+MavekGit@users.noreply.github.com>
2023-04-11 17:24:07 +0200

    Add files via upload

--- kalibracja_zdjec.py
+++ kalibracja_zdjec.py
@@ -10,10 +10,14 @@ img_points = []  # lista punktów na obrazie
 obj_points = []  # lista odpowiadających im punktów na planszy szachowej
 
 # Przetwarzanie obrazów
-for i in range(1, 24):
+for i in range(1, 23):
     # Wczytanie obrazu
     img_path = f"zdjecie ({i}).jpg"
     img = cv2.imread(img_path)
+    height, width = img.shape[:2]
+    new_height = int(height/6)
+    new_width = int(width/6)
+    img = cv2.resize(img, (new_width, new_height))
     if img is None:
         print(f"Nie udało się wczytać obrazu {img_path}.")
         continue
@@ -24,23 +28,23 @@ for i in range(1, 24):
 
     # Jeśli znaleziono punkty planszy szachowej
     if ret:
-        obj_points.append(np.zeros((COLS * ROWS, 3), np.float32))
-        obj_points[-1][:, :2] = np.mgrid[0:COLS, 0:ROWS].T.reshape(-1, 2)
-        img_points.append(corners)
+     obj_points.append(np.zeros((COLS * ROWS, 3), np.float32))
+     obj_points[-1][:, :2] = np.mgrid[0:COLS, 0:ROWS].T.reshape(-1, 2)
+     img_points.append(corners)
 
-        # Wizualizacja punktów planszy szachowej na obrazie
-        img = cv2.drawChessboardCorners(img, (COLS, ROWS), corners, ret)
+    # Wizualizacja punktów planszy szachowej na obrazie
+    img = cv2.drawChessboardCorners(img, (COLS, ROWS), corners, ret)
 
-        # Wyświetlanie obrazu
-        cv2.imshow('Calibration', img)
-        cv2.waitKey(0)
+    # Wyświetlanie obrazu
+    cv2.imshow('Calibration', img)
+    cv2.waitKey(100)
 
 cv2.destroyAllWindows()
 
 # Kalibracja kamery
-if len(obj_points) > 0:
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
-    np.savetxt('plik.txt', (ret, mtx, dist, rvecs, tvecs))
-    print("Kalibracja zakończona pomyślnie.")
-else:
-    print("Nie znaleziono punktów planszy szachowej na żadnym z obrazów.")
+# if len(obj_points) > 0:
+#     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
+#     np.savetxt('plik.txt', (ret, mtx, dist, rvecs, tvecs))
+#     print("Kalibracja zakończona pomyślnie.")
+# else:
+#     print("Nie znaleziono punktów planszy szachowej na żadnym z obrazów.")
