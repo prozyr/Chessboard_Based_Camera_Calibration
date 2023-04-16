5a147890614fa001ea32d1700507e2df3f06b509
tomdud-developer <tomadud045@student.polsl.pl>
2023-04-16 20:05:05 +0200

    Show images

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import numpy as np
 import glob
-import cv2 as cv
+import cv2
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 logging.info('Loading list of image files...')
-images = glob.glob('photos/Series1_*.jpg')
-logging.info("Number of found paths: " + str(len(images)))
+images_paths = glob.glob('photos/Series1_*.jpg')
+logging.info("Number of found paths: " + str(len(images_paths)))
 
+for img_path in images_paths:
+    img = cv2.imread(img_path)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+
+
+cv2.destroyAllWindows()
