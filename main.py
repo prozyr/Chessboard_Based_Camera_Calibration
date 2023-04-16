e64c750336945e96d4c444d161c2f817a191c6cc
tomdud-developer <tomadud045@student.polsl.pl>
2023-04-16 19:56:16 +0200

    Add photos and turn on logging

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-# Witaj tu powstanie nasz projekt.
-# Tom Branch
+import numpy as np
+import glob
+import cv2 as cv
+import logging
 
-print('Hello, CVaPR project!')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.info('Loading list of image files...')
+images = glob.glob('photos/Series1_*.jpg')
+logging.info("Number of found paths: " + str(len(images)))
+
