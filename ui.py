8b0a5aa44e2037f4bf0968e1ed8418cb12571cef
tomdud-developer <tomadud045@student.polsl.pl>
2023-06-11 17:53:50 +0200

    Wrap UI into class

--- ui.py
+++ ui.py
@@ -3,51 +3,56 @@ import cv2
 import tkinter as tk
 from PIL import Image, ImageTk
 
-window = tk.Tk()
-window.wm_title("Camera calibration")
-window.config(background="#FFFFFF")
+class CameraApp:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.wm_title("Camera calibration")
+        self.window.config(background="#FFFFFF")
 
-imageFrame = tk.Frame(window, width=600, height=500)
-imageFrame.grid(row=0, column=0, padx=10, pady=2)
+        self.imageFrame = tk.Frame(self.window, width=600, height=500)
+        self.imageFrame.grid(row=0, column=0, padx=10, pady=2)
 
-cap = cv2.VideoCapture(1)
-counter = 0  # Licznik
+        self.cap = cv2.VideoCapture(1)
+        self.counter = 0  # Licznik
 
-def show_frame():
-    global counter
-    _, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-    img = Image.fromarray(cv2image)
-    imgtk = ImageTk.PhotoImage(image=img)
-    display1.imgtk = imgtk
-    display1.configure(image=imgtk)
-    display2.imgtk = imgtk
-    display2.configure(image=imgtk)
+        self.display1 = tk.Label(self.imageFrame)
+        self.display1.grid(row=1, column=0, padx=10, pady=2)  # Display 1
+        self.display2 = tk.Label(self.imageFrame)
+        self.display2.grid(row=1, column=1, padx=10, pady=2)  # Display 2
 
-    counter += 1
-    print("Licznik:", counter)
+        self.counter_label = tk.Label(self.window, text="Licznik: 0")
+        self.counter_label.grid(row=0, column=0, padx=10, pady=2)
 
-    window.after(10, show_frame)
+        self.start_button = tk.Button(self.window, text="Start", command=self.start_button_click)
+        self.start_button.grid(row=0, column=1, padx=10, pady=2)
 
-def start_button_click():
-    global counter
-    counter = 0
-    print("Licznik zresetowany!")
+        self.sliderFrame = tk.Frame(self.window, width=600, height=100)
+        self.sliderFrame.grid(row=600, column=0, padx=10, pady=2)
 
-def show_frame_wrapper():
-    show_frame()
+        self.show_frame_wrapper()
 
-display1 = tk.Label(imageFrame)
-display1.grid(row=1, column=0, padx=10, pady=2)  # Display 1
-display2 = tk.Label(imageFrame)
-display2.grid(row=0, column=0)  # Display 2
+    def show_frame_wrapper(self):
+        _, frame = self.cap.read()
+        frame = cv2.flip(frame, 1)
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        img = Image.fromarray(cv2image)
+        imgtk = ImageTk.PhotoImage(image=img)
+        self.display1.imgtk = imgtk
+        self.display1.configure(image=imgtk)
+        self.display2.imgtk = imgtk
+        self.display2.configure(image=imgtk)
 
-start_button = tk.Button(window, text="Start", command=start_button_click)
-start_button.grid(row=1, column=1, padx=10, pady=2)
+        self.counter += 1
+        self.counter_label.config(text="Licznik: {}".format(self.counter))
 
-sliderFrame = tk.Frame(window, width=600, height=100)
-sliderFrame.grid(row=600, column=0, padx=10, pady=2)
+        self.window.after(10, self.show_frame_wrapper)
 
-window.after(10, show_frame_wrapper)  # Uruchamiając show_frame_wrapper()
-window.mainloop()
+    def start_button_click(self):
+        self.counter = 0
+        print("Licznik zresetowany!")
+
+    def run(self):
+        self.window.mainloop()
+
+app = CameraApp()
+app.run()
