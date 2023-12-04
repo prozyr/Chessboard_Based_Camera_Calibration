1eee337b0190a1c600b81287d15c53ff83bd2c70
Your Name <you@example.com>
2023-12-04 21:16:52 +0100

    README update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from CameraApplication import  CameraApplication
 
 CHESSBOARD_SIZE = (10, 7)
-CAMERA_NUMBER = 1
-HEIGHT = int(1080 / 2.5)
-WIDTH = int(1920 / 2.5)
+CAMERA_NUMBER = 0
+HEIGHT = int(1080 /100)
+WIDTH = int(1920 / 100)
 app = CameraApplication(CHESSBOARD_SIZE, CAMERA_NUMBER, HEIGHT, WIDTH)
 app.run()
